30071258399e3dd6d0ecc9692ac54fd0879cbc32
Chris James <qui666@gmail.com>
2015-09-30 15:18:15 +0100

    Added some json packages and a lovely function to copy the file path

--- init.el
+++ init.el
@@ -148,3 +148,15 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize)
   (exec-path-from-shell-copy-env "GOPATH"))
+
+(defun my-put-file-name-on-clipboard ()
+  "Put the current file name on the clipboard"
+  (interactive)
+  (let ((filename (if (equal major-mode 'dired-mode)
+                      default-directory
+                    (buffer-file-name))))
+    (when filename
+      (with-temp-buffer
+        (insert filename)
+        (clipboard-kill-region (point-min) (point-max)))
+      (message filename))))
