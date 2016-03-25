94fdeb7f262f144f6441b809ab91afe32f986667
Chris James <qui666@gmail.com>
2016-03-25 07:30:12 +0000

    New theme

--- init.el
+++ init.el
@@ -160,3 +160,18 @@
         (insert filename)
         (clipboard-kill-region (point-min) (point-max)))
       (message filename))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2)
+ '(custom-safe-themes
+   (quote
+    ("a1289424bbc0e9f9877aa2c9a03c7dfd2835ea51d8781a0bf9e2415101f70a7e" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
