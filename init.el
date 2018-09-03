cfb055a85ce8393071b41d6f013a0f0eab4d2e1b
Chris James <qui666@gmail.com>
2018-09-03 10:23:06 +0100

    Put some roswell shizzle in

--- init.el
+++ init.el
@@ -172,3 +172,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; clisp bollocks
+(load (expand-file-name "~/.roswell/helper.el"))
+
+(setq inferior-lisp-program "ros -Q run")
